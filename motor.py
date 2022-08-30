bb311ade297bd1cdd6327a38557155c110d4152b
navidkpr <navid.pour@outlook.com>
2022-08-30 17:59:38 -0400

    fixed more typos

--- motor.py
+++ motor.py
@@ -28,26 +28,26 @@ def destroy():
     motorStop()
     GPIO.cleanup()
 
-def move(speed, direction, turn, radius=0.6):   # 0 < radius <= 1    
-    #speed = 100  
-    if direction == 'forward':  
-        if turn == 'right':  
-            motor_left(0, left_backward, int(speed*radius))  
-            motor_right(1, right_forward, speed)  
-        elif turn == 'left':  
-            motor_left(1, left_forward, speed)  
-            motor_right(0, right_backward, int(speed*radius))  
-        else:  
-            motor_left(1, left_forward, speed)  
-            motor_right(1, right_forward, speed)  
-    else:  
-        pass  
+# def move(speed, direction, turn, radius=0.6):   # 0 < radius <= 1    
+#     #speed = 100  
+#     if direction == 'forward':  
+#         if turn == 'right':  
+#             motor_left(0, left_backward, int(speed*radius))  
+#             motor_right(1, right_forward, speed)  
+#         elif turn == 'left':  
+#             motor_left(1, left_forward, speed)  
+#             motor_right(0, right_backward, int(speed*radius))  
+#         else:  
+#             motor_left(1, left_forward, speed)  
+#             motor_right(1, right_forward, speed)  
+#     else:  
+#         pass  
 
 if __name__ == "__main__":
     try:
         speed_set = 60  
         setup()  
-        move(speed_set, 'forward', 'no', 0.8)  
+        # move(speed_set, 'forward', 'no', 0.8)  
         time.sleep(1.3)  
         motorStop()  
         destroy()  
