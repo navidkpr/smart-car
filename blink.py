5d306901fb70abc28b8e17019681355c9cdacb65
navidkpr <navid.pour@outlook.com>
2022-08-30 17:29:48 -0400

    changed frequency

--- blink.py
+++ blink.py
@@ -1,7 +1,7 @@
 import Adafruit_PCA9685  
 import time
 pwm = Adafruit_PCA9685.PCA9685()  
-pwm.set_pwm_freq(50) # Set the frequency of the PWM signal  
+pwm.set_pwm_freq(600) # Set the frequency of the PWM signal  
 while 1: # Make the servo connected to the No. 3 servo port on the Robot HAT reciprocate
     pwm.set_pwm(0, 0, 300)
     time.sleep(1)
