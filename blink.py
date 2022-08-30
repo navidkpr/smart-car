37567b6fe26a4ae503bcd8b2405dc22ac2cb8cf7
navidkpr <navid.pour@outlook.com>
2022-08-30 17:18:49 -0400

    added files

--- blink.py
+++ blink.py
@@ -1,6 +1,6 @@
-import Adafruit_PCA9685 # Import the library used to communicate with PCA9685  
+import adafruit-circuitpython-pca9685 # Import the library used to communicate with PCA9685  
 import time    
-pwm = Adafruit_PCA9685.PCA9685() # Instantiate the object used to control the PWM  
+pwm = adafruit-circuitpython-pca9685.PCA9685() # Instantiate the object used to control the PWM  
 pwm.set_pwm_freq(50) # Set the frequency of the PWM signal  
 while 1: # Make the servo connected to the No. 3 servo port on the Robot HAT reciprocate
     pwm.set_pwm(0, 0, 300)
