50f34b6e1262b454c333d820730e4e2d836e32dd
navidkpr <navid.pour@outlook.com>
2022-08-30 17:06:53 -0400

    fixed compile error

--- blink.py
+++ blink.py
@@ -2,7 +2,7 @@ import Adafruit_PCA9685 # Import the library used to communicate with PCA9685
 import time    
 pwm = Adafruit_PCA9685.PCA9685() # Instantiate the object used to control the PWM  
 pwm.set_pwm_freq(50) # Set the frequency of the PWM signal  
-while 1 # Make the servo connected to the No. 3 servo port on the Robot HAT reciprocate
+while 1: # Make the servo connected to the No. 3 servo port on the Robot HAT reciprocate
     pwm.set_pwm(0, 0, 300)
     time.sleep(1)
     pwm.set_pwm(0, 0, 400)
